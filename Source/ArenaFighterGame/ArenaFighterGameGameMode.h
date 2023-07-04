d75874a2f15e2964af6ec6c4b8b6caaf34081aa1
Florian <florian5611@gmail.com>
2023-07-04 23:28:56 +0200

    push sources

---
+++ Source/ArenaFighterGame/ArenaFighterGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArenaFighterGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArenaFighterGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArenaFighterGameGameMode();
+};
+
+
+
