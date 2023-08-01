4d37696d259f64008ac55fa3e59f708c9b99a974
Florian <florian5611@gmail.com>
2023-08-01 23:07:18 +0200

    Dash in 4 directions but floor referential, need conversions

--- Source/ArenaFighterGame/ArenaFighterGameCharacter.cpp
+++ Source/ArenaFighterGame/ArenaFighterGameCharacter.cpp
@@ -28,7 +28,7 @@ AArenaFighterGameCharacter::AArenaFighterGameCharacter()
 	WalkingSpeed = 400.f;
 	RunningSpeed = 800.f;
 
-	DashDistance = 1500.0f;
+	DashDistance = 500.0f;
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
