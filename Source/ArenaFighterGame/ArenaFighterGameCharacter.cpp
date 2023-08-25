1303a064aa42fbd0b03d07c48b7b07d86ee6408b
Florian <florian5611@gmail.com>
2023-08-25 20:12:13 +0200

    end cleaning and next will change context when switch lock/unlock camera

--- Source/ArenaFighterGame/ArenaFighterGameCharacter.cpp
+++ Source/ArenaFighterGame/ArenaFighterGameCharacter.cpp
@@ -163,11 +163,6 @@ void AArenaFighterGameCharacter::Move(const FInputActionValue& Value)
 
 void AArenaFighterGameCharacter::Look(const FInputActionValue& Value)
 {
-	if (bIsCameraLockedOnEnemy)
-	{
-		ChangePosture(Value);///////////////// NEED TO THINK ABOUT IT
-		return;
-	}
 	// input is a Vector2D
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
